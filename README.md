0526d6b083cce9efa2d928ff2a5680c27fa13e12
Pamento <pamento.dev@gmail.com>
2017-11-21 23:17:31 +0100

    second design

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-
-The beginning of the path, can be difficult and very exciting in the same time.
-At first look everything seems very limited. But only at the beginning.
-
-translation of: 'Nie rzuca słów na wiatr'
--> 'He does not throw words to the wind'
+Some designs draw in the console by help of javascript.
--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,23 @@ function drawTree() {
   }
 }
 drawTree();
+console.log(' ');
 
-
+// draw 8
+for(var i = 0; i<5; i++){
+    var star='';
+    if((i===0)||(i===2)||(i===4)){
+        for(var j=0; j<5;j++){
+            star += ' *';}
+    } else {
+        for(var k=0; k<8; k++){
+            if((k===0)||(k===7)){
+                star+=' *';
+            } else {
+                star+=' ';
+            }
+        }
+    }
+    console.log(star);
+}
 
